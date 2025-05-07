836867dc3300720904d0ea186687d8b4c98bbe66
noura <noura.tu19@gmail.com>
2025-05-07 16:31:21 +0300

    Dockerfile

--- shareskills/src/utilities/sendRequest.js
+++ shareskills/src/utilities/sendRequest.js
@@ -1,23 +1,31 @@
+// src/utilities/sendRequest.js
 export default async function sendRequest(url, method = 'GET', payload) {
-	const token = localStorage.getItem('token');
-	const options = { method };
-  
-	if (payload) {
-		options.headers = { 'Content-Type': 'application/json' };
-		options.body = JSON.stringify(payload);
-	}
+  const token = localStorage.getItem('token');
+  const headers = {};
 
-	if (token) {
-        options.headers = options.headers || {};
-        options.headers.Authorization = `Bearer ${token}`;
-    }
+  let body;
+  if (payload instanceof FormData) {
+    // Let fetch set Content-Type: multipart/form-data; boundary=…
+    body = payload;
+  } else if (payload != null) {
+    // JSON payloads
+    headers['Content-Type'] = 'application/json';
+    body = JSON.stringify(payload);
+  }
 
-	try {
-		const res = await fetch(`http://127.0.0.1:8000${url}`, options);
-		if (res.ok) return res.json();
-	} catch (err) {
-		console.log(err, "error in send-request");
-		return err;
-	}
-    
+  if (token) {
+    headers['Authorization'] = `Bearer ${token}`;
+  }
+
+  const res = await fetch(`http://127.0.0.1:8000${url}`, {
+    method,
+    headers,
+    body,
+  });
+
+  if (!res.ok) {
+    const errorBody = await res.json().catch(() => ({}));
+    throw errorBody;
+  }
+  return res.json();
 }
